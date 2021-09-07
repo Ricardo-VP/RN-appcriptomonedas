622d7f1f7752591cafd86968b80440133d3c0090
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-09-07 18:56:46 -0500

    Add: carpeta de assets y carpeta de componentes

---
+++ components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, StyleSheet, Platform} from 'react-native'
+const Header = () => (  
+        <Text style={styles.encabezado}>Criptomonedas</Text>
+    );
+ 
+const styles = StyleSheet.create({
+    encabezado: {
+        paddingTop: Platform.OS === 'ios' ? 50 : 20
+    }
+});
+
+export default Header;
