1010dd2552cc26c8d545b5c8defe85f687dc5cd9
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-09-07 19:13:04 -0500

    Terminando el header

--- components/header.js
+++ components/header.js
@@ -7,7 +7,14 @@ const Header = () => (
 const styles = StyleSheet.create({
     encabezado: {
         paddingTop: Platform.OS === 'ios' ? 50 : 20,
-        fontFamily: 'Lato-Black'
+        fontFamily: 'Lato-Black',
+        backgroundColor: '#5E49E2',
+        paddingBottom: 10,
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        fontSize: 20,
+        color: '#FFF',
+        marginBottom: 30
     }
 });
 
