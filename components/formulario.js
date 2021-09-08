676b766bed1ab5dc024095d59b926eeb0e3bcb9a
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-09-07 19:56:14 -0500

    Add: Picker de criptomonedas

--- components/formulario.js
+++ components/formulario.js
@@ -9,21 +9,27 @@ const Formulario = () => {
 
   useEffect(() => {
     const consultarAPI = async () => {
-        const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
-        const resultado = await axios.get(url);
-        setCriptomonedas(resultado.data.Data);
-    }
+      const url =
+        'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+      const resultado = await axios.get(url);
+      setCriptomonedas(resultado.data.Data);
+    };
     consultarAPI();
   }, []);
+  // ALMACENA LAS SELECCIONES DEL USUARIO
   const obtenerMoneda = moneda => {
     setMoneda(moneda);
+  };
+  const obtenerCriptomoneda = cripto => {
+    setCriptomoneda(cripto);
   };
   return (
     <View>
       <Text style={styles.label}>Moneda</Text>
       <Picker
         selectedValue={moneda}
-        onValueChange={moneda => obtenerMoneda(moneda)}>
+        onValueChange={moneda => obtenerMoneda(moneda)}
+        itemStyle={{height: 120}}>
         <Picker.Item label="- Seleccione -" value="" />
         <Picker.Item label="Dolar Estadounidense" value="USD" />
         <Picker.Item label="Peso Mexicano" value="MXN" />
@@ -31,6 +37,19 @@ const Formulario = () => {
         <Picker.Item label="Libra Esterlina" value="GBP" />
       </Picker>
       <Text style={styles.label}>Criptomoneda</Text>
+      <Picker
+        selectedValue={criptomoneda}
+        onValueChange={criptomoneda => obtenerCriptomoneda(criptomoneda)}
+        itemStyle={{height: 120}}>
+        <Picker.Item label="- Seleccione -" value="" />
+        {criptomonedas.map(cripto => (
+          <Picker.Item
+            key={cripto.CoinInfo.Id}
+            label={cripto.CoinInfo.FullName}
+            value={cripto.CoinInfo.Name}
+          />
+        ))}
+      </Picker>
     </View>
   );
 };
