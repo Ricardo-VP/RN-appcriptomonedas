d70903130fae20b5ef1c7afaa9e2b680a6cb5d31
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-09-07 19:15:34 -0500

    Add: imagen de criptomonedas

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {} from 'react-native';
+import {StyleSheet, Image} from 'react-native';
 import Header from './components/header';
 const App = () => {
   return (
     <>
       <Header />
+      <Image 
+        style={styles.imagen}
+        source={require('./assets/img/cryptomonedas.png')}
+      />
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  imagen: {
+    width: '100%',
+    height: 150,
+    marginHorizontal: '2.5%'
+  }
+})
 
 export default App;
